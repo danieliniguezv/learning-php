38e8995cf9f85753f0a0b2829ff6d7f889365e56
Daniel Iñiguez <danieliniguezv@MacBook-Pro-de-Daniel.local>
2024-06-02 17:19:37 -0600

    feat: added associative arrays grades programs.

---
+++ arrays/associative/example-1/grades.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Associative Array Grades</title>
+</head>
+<body>
+	<?php 
+		$grades = array("Daniel" => 10, "Roberto" => 9, "Rocky" => 10);
+		echo $grades["Daniel"];
+	?>
+</body>
+</html>
