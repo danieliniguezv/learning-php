95d99f1db436bda9c4b8692d1893d14652cbd14b
Daniel Iñiguez <danieliniguezv@MacBook-Pro-de-Daniel.local>
2024-06-03 19:46:08 -0600

    feat: added dynamic php include files

---
+++ include/php/blog_include.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Include PHP | Blog</title>
+</head>
+<body>
+	<?php 
+		$title = "Kanurio Lávaro";
+		$author = "Alfajor Pelóntido Doblado";
+		$wordCount = 450;
+		include "article-header.php" 
+	?>
+</body>
+</html>
